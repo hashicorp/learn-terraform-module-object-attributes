6420bfbf2baa822ec71c831732a0d45c78887de6
Robin Norwood <rnorwood@hashicorp.com>
2023-11-09 12:05:37 -0600

    Update S3 bucket config and provider version

--- modules/aws-s3-static-website/main.tf
+++ modules/aws-s3-static-website/main.tf
@@ -22,10 +22,30 @@ resource "aws_s3_bucket_website_configuration" "web" {
   }
 }
 
-resource "aws_s3_bucket_acl" "web" {
+resource "aws_s3_bucket_ownership_controls" "web" {
+  bucket = aws_s3_bucket.web.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "web" {
   bucket = aws_s3_bucket.web.id
 
-  acl = "public-read"
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "web" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.web,
+    aws_s3_bucket_public_access_block.web,
+  ]
+
+  bucket = aws_s3_bucket.web.id
+  acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "web" {
--- terraform.tf
+++ terraform.tf
@@ -11,9 +11,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.30.0"
+      version = "~> 5.24.0"
     }
   }
 
-  required_version = ">= 1.3"
+  required_version = "~> 1.3"
 }
